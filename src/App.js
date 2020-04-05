06ab32638be59590a6dddd79276fca233cee6bff
George Travis <wewerelikesharks@gmail.com>
2020-04-05 11:19:43 -0700

    column headers in place

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router /*, Route*/ } from 'react-router-dom';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
 import Navbar from './components/Navbar';
 import Container from './components/Container';
+import ColumnHeaders from './components/ColumnHeaders';
 import './App.css';
 // import Discover from './pages/Discover';
 // import About from './pages/About';
 // import Search from './pages/Search';
 // import Footer from './components/Footer';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Wrapper>
-          <Navbar />
-          <Container>Inside container</Container>
-        </Wrapper>
-        {/* <Navbar />
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Wrapper>
+            <Navbar />
+            <Container>
+              <ColumnHeaders />
+            </Container>
+          </Wrapper>
+          {/* <Navbar />
         <Wrapper>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
@@ -27,9 +31,10 @@ function App() {
           <Route exact path="/search" component={Search} />
         </Wrapper>
         <Footer /> */}
-      </div>
-    </Router>
-  );
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ColumnHeaders/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+
+function ColumnHeaders() {
+  return (
+    <div className="column-headers">
+      <div>
+        <p>Image</p>
+      </div>
+      <div>
+        <p>Name</p>
+      </div>
+      <div>
+        <p>Phone</p>
+      </div>
+      <div>
+        <p>Email</p>
+      </div>
+      <div>
+        <p>DOB</p>
+      </div>
+    </div>
+  );
+}
+
+export default ColumnHeaders;
